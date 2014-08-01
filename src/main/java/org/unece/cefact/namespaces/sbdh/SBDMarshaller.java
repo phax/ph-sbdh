b7558da7675cda1165b1056b858fd4d14d61f9e5
phax <ph@phloc.com>
2014-08-01 14:01:48 +0200

    Updated license headers

--- src/main/java/org/unece/cefact/namespaces/sbdh/SBDMarshaller.java
+++ src/main/java/org/unece/cefact/namespaces/sbdh/SBDMarshaller.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (C) 2013-2014 Philip Helger
- * ph[at]phloc[dot]com
+ * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
